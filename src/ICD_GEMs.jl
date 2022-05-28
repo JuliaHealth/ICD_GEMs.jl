fb5f6e265995c668fe858f67fa13e37bb9e0c9aa
Interdisciplinary Physics Team (InPhyT) <63546437+InterdisciplinaryPhysicsTeam@users.noreply.github.com>
2022-05-28 16:07:17 +0200

    Minor doc fix
    Co-Authored-By: Pietro Monticone <38562595+pitmonticone@users.noreply.github.com>
    Co-Authored-By: Claudio Moroni <43729990+ClaudMor@users.noreply.github.com>

--- src/ICD_GEMs.jl
+++ src/ICD_GEMs.jl
@@ -166,7 +166,7 @@ function get_ICD_code_range(range::String, revision::String)
 end
 
 """
-    execute_applied_mapping(GEM::GEM{OrderedDict{String, OrderedDict{String, Any}}}, source_codes::Vector{String}, applied_mapping::String)
+    execute_applied_mapping(GEM::GEM{OrderedDict{String, OrderedDict{String, Any}}}, source_codes::Vector{String})
 
 Translate each element of `source_codes` to the target international classification contained in `GEM` using `GEM`.
 """
